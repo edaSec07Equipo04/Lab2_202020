05e21023b9527f323b67b388d2051b7c2acdb212
sebastianpenaandes <j.penam@uniandes.edu.co>
2020-08-25 20:52:14 -0500

    j.penam

--- Test/Lab 2/test_insertionsortmovies.py
+++ Test/Lab 2/test_insertionsortmovies.py
@@ -10,15 +10,15 @@ from DataStructures import listiterator as it
 from ADT import list as lt
 import csv
 
-#list_type = 'ARRAY_LIST'
-list_type = 'SINGLE_LINKED'
+list_type = 'ARRAY_LIST'
+#list_type = 'SINGLE_LINKED'
 
 lst_moovie = lt.newList(list_type)
-mooviefile = cf.data_dir + 'theMoviesdb/AllMoviesDetailsCleaned 2.csv'
+mooviefile = cf.data_dir + 'theMoviesdb/AllMoviesDetailsCleaned2.csv'
 
 
 def setUp():
-    print('Loading books')
+    print('Loading movies')
     loadCSVFile(mooviefile, lst_moovie)
     print(lst_moovie['size'])
 
@@ -27,10 +27,14 @@ def tearDown():
        pass
 #vote_average;vote_count
 
-def loadCSVFile(file, lst):
-    input_file = csv.DictReader(open(file, encoding = "utf-8"))
-    for row in input_file:
-        lt.addLast(lst, row)
+def loadCSVFile(file, lst, sep=';'):
+    dialect = csv.excel()
+    dialect.delimiter=sep
+    try:
+        with open(file, encoding="utf-8") as csvfile:
+            spamreader = csv.DictReader(csvfile, dialect=dialect)
+            for row in spamreader: 
+                lt.addLast(lst,row)
 
 def printList(lst):
     iterator = it.newIterator(lst)
@@ -39,7 +43,7 @@ def printList(lst):
         print(element['vote_average'])
 
 def less(element1, element2):
-    if int(element1['vote_average']) < int(element2['vote_average']):
+    if float(element1['vote_average']) < float(element2['vote_average']):
         return True
     return False
 
@@ -57,9 +61,9 @@ def test_loading_CSV_y_ordenamiento():
     setUp()
     sort.insertionSort(lst_moovie,less)
     while not (lt.isEmpty(lst_moovie)):
-        x = int(lt.removeLast(lst_moovie)['vote_average'])
+        x = float(lt.removeLast(lst_moovie)['vote_average'])
         if not (lt.isEmpty(lst_moovie)):
-            y = int(lt.lastElement(lst_moovie)['vote_average'])
+            y = float(lt.lastElement(lst_moovie)['vote_average'])
         else:
             break
-        assert x > y
+        assert x >= y
